80d9783cadf394f459191421ebe7955265528d88
Lévai Balázs <levaibalazsistvan@ktch.hu>
2024-09-13 08:57:28 +0200

    Data/Navbar/Service

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DataComponent } from './data/data.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    DataComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
